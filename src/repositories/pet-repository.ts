a9a3efbbc121d87bfc28eff8be73ef0e3703e03c
Davi Thomeny Cotting <khonshu23@gmail.com>
2025-07-14 14:21:47 -0300

    feat: create and test of get pet details use case

--- src/repositories/pet-repository.ts
+++ src/repositories/pet-repository.ts
@@ -2,4 +2,5 @@ import { Pet, Prisma } from "@/generated/prisma";
 
 export interface PetsRepository {
     create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
+    findById(id: string): Promise<Pet | null>
 }
